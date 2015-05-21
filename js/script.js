f4eeb2cd5c456b2b85031add8c5bd41d3e5ce48b
Galen Scovell <galen.scovell@gmail.com>
2015-05-21 16:40:31 -0700

    Implement Line/Point animations
    Now let's see how much I can clean up...

--- js/script.js
+++ js/script.js
@@ -1,49 +1,21 @@
 
-/******************************************************************************
-    Connect the Dots | Galen Scovell, 05/21/15
-
-    Connecting points:
-    1) Find point n -> point with largest y-coordinate.
-    2) Sort all other points by their polar angle with n.
-    3) Connect points in order.
-
-    Complexity: 
-    Outside of sorting, time complexity is [O(3n)] since we iterate over one
-    points Array three separate times. Space complexity is O(n) as we only 
-    ever have one Array of points in memory.
-
-    JavaScript built-in array.sort() utilizes either mergesort or quicksort
-    depending on browser. Time complexity either way is [O(log(n))], space 
-    either [O(n)] (mergesort) or [O(log(n))] (quicksort).
-******************************************************************************/
+/*************************************************
+    Connect the Dots | Galen Scovell, 05/21/15      
+**************************************************/
 
 
-context = $('#canvas')[0].getContext("2d");
-points = new Array();
-requestAnimationFrame = window.requestAnimationFrame || 
-                        window.mozRequestAnimationFrame || 
-                        window.webkitRequestAnimationFrame || 
-                        window.msRequestAnimationFrame;
+var context;
+var points, lines, drawnLines;
+var currentLine, linesCreated;
+var requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame || 
+                            window.webkitRequestAnimationFrame || 
+                            window.msRequestAnimationFrame;
 
 
 /*******************************
     POINT FUNCTIONALITY
 *******************************/
-function drawLines(startingPoint) {
-    var previousPoint = startingPoint;
-    for (var i = 1; i < points.length; i++) {
-        context.beginPath();
-        context.moveTo(previousPoint.x, previousPoint.y);
-        context.lineTo(points[i].x, points[i].y);
-        context.stroke();
-        previousPoint = points[i];
-    }
-    context.beginPath();
-    context.moveTo(previousPoint.x, previousPoint.y);
-    context.lineTo(startingPoint.x, startingPoint.y);
-    context.stroke();
-}
-
 function connectPoints() {
     var startingPoint = findStartingPoint();
     for (var i = 0; i < points.length; i++) {
@@ -51,12 +23,16 @@ function connectPoints() {
         points[i].angle = angleFromStart;
     }
     points.sort(angleComparison);
-    printPoints();
-    // drawLines(startingPoint);
+    var previousPoint = startingPoint;
+    for (var i = 1; i < points.length; i++) {
+        createLine(points[i], previousPoint);
+        previousPoint = points[i];
+    }
+    createLine(startingPoint, points[points.length - 1]);
+    linesCreated = true;
 }
 
 function findStartingPoint() {
-    // Find point with largest y, set as starting point and remove from points array
     var largestY = 0;
     var startingPoint;
     for (var i = 0; i < points.length; i++) {
@@ -65,14 +41,26 @@ function findStartingPoint() {
             largestY = startingPoint.y;
         }
     }
-    // points.splice(points.indexOf(startingPoint), 1);
     startingPoint.state = 1;
     startingPoint.frame = 200;
     return startingPoint;
 }
 
-function drawPoint(pointX, pointY) {
-    points.push({x:pointX, y:pointY, angle:0, frame:200, state:0});
+function createPoint(pointX, pointY) {
+    points.push({x: pointX, y: pointY, angle: 0, frame: 200, state: 0});
+}
+
+function createLine(thisPoint, previousPoint) {
+    var lineSlope = (thisPoint.y - previousPoint.y) / (thisPoint.x - previousPoint.x);
+    var lineSections = Math.abs(thisPoint.x - previousPoint.x) / 10;
+    lines.push({startX: previousPoint.x,
+                startY: previousPoint.y,
+                currentX: previousPoint.x, 
+                currentY: previousPoint.y,
+                endX: thisPoint.x,
+                endY: thisPoint.y,
+                slope: lineSlope,
+                sections: lineSections});
 }
 
 
@@ -81,31 +69,90 @@ function drawPoint(pointX, pointY) {
 *******************************/
 function render() {
     context.clearRect(0, 0, canvas.width, canvas.height);
+    if (linesCreated && currentLine < lines.length) {
+        animateLine(lines[currentLine]);
+    }
+    renderLines();
+    renderPoints();
+    requestAnimationFrame(render);
+}
+
+function renderLines() {
+    context.shadowBlur = 3;
+    context.shadowColor = '#8e44ad';
+    context.strokeStyle = '#9b59b6';
+    context.lineWidth = 4;
+    for (var i = 0; i < drawnLines.length; i++) {
+        context.beginPath();
+        context.moveTo(drawnLines[i].startX, drawnLines[i].startY)
+        context.lineTo(drawnLines[i].endX, drawnLines[i].endY);
+        context.stroke();
+    }
+}
+
+function renderPoints() {
     for (var i = 0; i < points.length; i++) {
         if (points[i].state == 0) {
-            context.shadowBlur = 3;
             context.shadowColor = '#2c3e50';
             context.fillStyle = '#34495e';
         } else if (points[i].state == 1) {
-            context.shadowBlur = 3;
             context.shadowColor = '#2980b9';
             context.fillStyle = '#3498db';
         }
+        context.shadowBlur = 3;
         context.beginPath();
-        context.arc(points[i].x, points[i].y, points[i].frame / 10, 0, 2*Math.PI);
+        context.arc(points[i].x, points[i].y, points[i].frame / 10, 0, 2 * Math.PI);
         context.closePath();
         context.fill();
         if (points[i].frame - 8 > 100) {
             points[i].frame -= 8;
         }
     }
-    requestAnimationFrame(render);
+}
+
+function animateLine(line) {
+    context.shadowBlur = 3;
+    context.shadowColor = '#8e44ad';
+    context.strokeStyle = '#9b59b6';
+    context.lineWidth = 4;
+    context.beginPath();
+    context.moveTo(line.startX, line.startY);
+    if (line.currentX < line.endX) {
+        line.currentX += line.sections;
+        line.currentY += line.slope * line.sections;
+    } else {
+        line.currentX -= line.sections;
+        line.currentY -= line.slope * line.sections;
+    }
+    context.lineTo(line.currentX, line.currentY);
+    context.stroke();
+    if ((line.startX < line.endX && line.currentX >= line.endX) || (line.startX > line.endX && line.currentX <= line.endX)) {
+        currentLine++;
+        if (currentLine < points.length) {
+            points[currentLine].state = 1;
+            points[currentLine].frame = 200;
+        }
+        drawnLines.push(line);
+    }
 }
 
 
 /*******************************
     HELPER FUNCTIONS
 *******************************/
+function initCanvas() {
+    $('#canvas').attr('height', window.innerHeight * 0.6);
+    $('#canvas').attr('width', window.innerWidth * 0.8);
+    context = $('#canvas')[0].getContext("2d");
+    points = new Array();
+    lines = new Array();
+    drawnLines = new Array();
+    currentLine = 0;
+    linesCreated = false;
+    $('#solve_button').text('Solve');
+    render();
+}
+
 function printPoints() {
     for (var i = 0; i < points.length; i++) {
         console.log(points[i].x + ", " + points[i].y + ", " + points[i].angle);
@@ -131,9 +178,7 @@ $('#solve_button').click(function(e) {
         connectPoints();
         $(this).text('Reset');
     } else if ($(this).text() == 'Reset') {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        points = new Array();
-        $(this).text('Solve');
+        initCanvas();
     }
 })
 
@@ -141,11 +186,13 @@ $('#canvas').click(function(e) {
     var offset = $(this).offset();
     var clickX = e.pageX - offset.left;
     var clickY = e.pageY - offset.top;
-    drawPoint(clickX, clickY);
+    createPoint(clickX, clickY);
+})
+
+$(window).resize(function() {
+    initCanvas();
 })
 
 $(document).ready(function() {
-    $('#canvas').attr('height', $('#canvas').css('height'));
-    $('#canvas').attr('width', $('#canvas').css('width'));
-    render();
+    initCanvas();
 })
